8d6d4a12a982caa281f69cc95249eb7a115303f7
Atchu <hanumaiah061@gmail.com>
2024-04-24 12:02:47 +0530

    common.sh v1

---
+++ common.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+########################################
+#
+# Author : Hanu
+# Date   : 24-04-2024
+#
+#########################################
+
+# Declaring variables
+USERID=$(id -u);
+
+# Declaring functions
+check_root() 
+{
+    if [ $USERID -ne 0 ]
+    then 
+    echo "Please run script with admin"
+    else
+    echo "Admin,proceed"
+    fi
+}
